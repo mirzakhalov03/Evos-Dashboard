1acab58b92c408cc706896ce01e1f743052c4895
mirzakhalov03 <javohirmirzakhalov@gmail.com>
2024-07-18 11:46:38 +0500

    edited employees section

--- src/routers/employees/Employees.jsx
+++ src/routers/employees/Employees.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Employees = () => {
   return (
-    <div>Employees</div>
+    <div style={{textAlign: 'center'}}>
+      <h1>No Employee data to show</h1>
+      <br />
+      <p><b>Wait until the administrator adds employees</b></p>
+      <br />
+      <p>Available soon...</p>
+    </div>
+
   )
 }
 
